dd063aed00f1049e1e834d33703cfd0b90c775ad
VoMinhLoi <minhloi1131130@gmail.com>
2024-12-02 09:16:03 +0700

    Morgan: HTTP logger

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // "start": "node --watch --inspect index.js",
 // "start": "nodenom --inspect index.js",
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+app.use(morgan())
+// app.use(morgan('combined'))
 app.get('/home',(request, response)=>response.send('Get all users'))
 const port = 3000
 

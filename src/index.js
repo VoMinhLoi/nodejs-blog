95d12b949d8f5cf41253706001c9272f51e261da
VoMinhLoi <minhloi1131130@gmail.com>
2024-12-02 16:55:15 +0700

    Test Method POST

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan())
+// app.use(morgan())
 // app.use(morgan('combined'))
 
 // Template engine
@@ -22,6 +22,18 @@ app.set('view engine', 'handlebars')//Thông báo với Express rằng mọi fil
 app.set('views', path.join(__dirname, 'resources/views'))
 app.get('/',(request, response)=>response.render('home'))
 app.get('/news',(request, response)=>response.render('news'))
+app.get('/search',(request, response)=>{
+    response.render('search')
+})
+
+app.use(express.urlencoded({
+    extended: true
+})) // Middleware mã hóa dữ liệu từ form data client gửi lên và gán cho đối tượng request.body 
+// app.use(express.json()) // JSON.parse() từ json sang javascript type khi dùng fetch,  
+app.post('/search',(request, response)=>{
+    console.log(request.body);
+    // response.render('search')
+})
 
 const port = 3000
-app.listen(port, ()=>{console.log(`Example app listening http://localhost:${port}`)})
+app.listen(port, ()=>{console.log(`http://localhost:${port}`)})
